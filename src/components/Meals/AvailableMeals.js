407db79170dc7b779a52da17eabd52eeecf42d37
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-02 11:08:15 +0200

    connect to firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,12 +6,19 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState(null);
 
 	useEffect(() => {
 		const fetchMeals = async () => {
 			const response = await fetch(
 				'https://react-http-ecb4c-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
 			);
+
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+
 			const responseData = await response.json();
 			const loadedMeals = [];
 
@@ -20,10 +27,30 @@ const AvailableMeals = () => {
 			}
 
 			setMeals(loadedMeals);
+			setIsLoading(false);
 		};
 
-		fetchMeals();
+		fetchMeals().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error.message);
+		});
 	}, []);
+
+	if (isLoading) {
+		return (
+			<section className={classes['meals-loading']}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<section className={classes['meals-error']}>
+				<p>{httpError}</p>
+			</section>
+		);
+	}
 
 	const mealsList = meals.map((meal) => (
 		<MealItem
